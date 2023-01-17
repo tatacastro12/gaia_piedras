32f995d7fdb831c45277e9c822e9afbb6cdf78fa
tatacastro12 <91699554+tatacastro12@users.noreply.github.com>
2023-01-17 11:04:43 -0800

    cambios

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Admin from "./pages/admin/Admin";
 //pages
-import {Home, Contact} from "./pages/";
+import { Home, Contact } from "./pages/";
 //componets
-import { Footer, Header } from "./components";
-
+import { Footer, Header } from "./components/";
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/contact" element={<Contact/>}/>
-        <Route path="/admin" element={<Admin/>}/>
-    </Routes>
-      <Footer/>
-    </BrowserRouter>
- 
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,25 +1,21 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 import "./Header.scss";
-
-
 
 const Header = () => {
   return (
-    <Header className= "header" >
-        <div className='logo'>
+    <Header className="header">
+      <div className="logo">
         <div>
-        <Link to="/">
-          <h2>
-          G <span>aia</span>.
-          </h2>
-        </Link>
-
+          <Link to="/">
+            <h2>
+              G <span>aia</span>.
+            </h2>
+          </Link>
         </div>
-
-        </div>
+      </div>
     </Header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
