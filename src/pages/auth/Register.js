f131fc4f136c4a5152689686ad43042068120e56
tatacastro12 <91699554+tatacastro12@users.noreply.github.com>
2023-01-19 13:44:02 -0800

    creacion firebase

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import "./Auth.scss";
+import { Link, Outlet } from "react-router-dom"
+import Card from "../../components/card/Card";
+import  registerImg from "../../components/assets/register.jpg";
+
 
 const register = () => {
   return (
-    <div>register</div>
-  )
-}
+    <>
+      <section className="auth container">
+        <Card>
+          <div className="form">
+            <h2>Register</h2>
+            <form>
+              <input type="text" placeholder="Email" required />
+              <input type="Password" placeholder="Password" required />
+              <input type="Password" placeholder=" Confirm Password" required />
+              <button className="--btn --btn-primary --btn-block">Register</button>
+            </form>
+            
+            <span className="register">
+              <p>Already an account ?</p>
+              <Link to="/login">Login</Link>
+            </span>
+          </div>
+        </Card>
+        <div className="img">
+          <img src={registerImg} alt="Register" width={400} />
+        </div>
+      </section>
+      <Outlet />
+    </>
+  );
+};
 
-export default register
+export default register;
--- src/pages/auth/Reset.js
+++ src/pages/auth/Reset.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import "./Auth.scss";
+import resetImg from "../../components/assets/reset.jpg";
+import { Link, Outlet } from "react-router-dom";
+import Card from "../../components/card/Card";
 
 const Reset = () => {
   return (
-    <div>Ree</div>
-  )
-}
+    <>
+      <section className="auth container">
+        <div className="img">
+          <img src={resetImg} alt="reset" width={400} />
+        </div>
+        <Card>
+          <div className="form">
+            <h2>Reset Password</h2>
+            <form>
+              <input type="text" placeholder="Email" required />
+              <button className="--btn --btn-primary --btn-block">
+                Reset Password
+              </button>
+              <div className="links">
+              <p>
+              <Link to="/login"> -Login</Link>
+              <Link to="/register"> -Register</Link>
+              </p>
+              </div>
+            </form>
+          </div>
+        </Card>
+      </section>
+      <Outlet />
+    </>
+  );
+};
 
-export default Reset
+export default Reset;
